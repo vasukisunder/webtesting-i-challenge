5e82592a64cf1e8c4f551f7b5816d0928d68f658
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-05-20 04:23:58 -0700

    completed mvp and stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,41 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const newItem = item;
+  if (newItem.enhancement < 20){
+   ++newItem.enhancement;
+  }
+  return newItem;
 }
 
 function fail(item) {
-  return { ...item };
+  let enhancement = item.enhancement;
+  let durability = item.durability;
+  if (enhancement < 15) {
+    durability = durability - 5;
+  } else {
+    durability = durability - 10
+    if (enhancement >= 17) {
+      --enhancement;
+    }
+  } 
+  return { ...item, enhancement, durability };
 }
 
+
 function repair(item) {
-  return { ...item };
+  const newItem = item;
+  newItem.durability = 100;
+  return newItem;
 }
 
 function get(item) {
-  return { ...item };
+  let {name, enhancement} = item;
+  const newItem = item;
+  if (enhancement > 0){
+    newItem.name= `[+${enhancement}] ${name}`
+  }
+  console.log(newItem);
+  return item;
 }
+
